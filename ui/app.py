4fb44f725cf2fdede2594e8554e7be7281077c2f
leod <subtle.frustration@proton.me>
2019-02-16 20:32:41 +0100

    Fix tab

--- ui/app.py
+++ ui/app.py
@@ -30,7 +30,7 @@ def gen():
 
 @app.route('/')
 def index():
-	return app.send_static_file('index.html')
+  return app.send_static_file('index.html')
 
 if __name__ == '__main__':
   app.run(debug=True, host='0.0.0.0')
