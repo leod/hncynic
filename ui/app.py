d54b5f9aae835e07f8e51baaa39cc948d96859a7
leod <subtle.frustration@proton.me>
2019-02-17 01:33:32 +0100

    Add readme stub for UI

--- ui/app.py
+++ ui/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__, static_url_path='/static')
 
 HOST='localhost'
 PORT=9000
-MODEL_NAME='1550321776'
+MODEL_NAME='1550362227'
 PREPROCESSOR='../data/preprocess.sh'
 POSTPROCESSOR='../data/mosesdecoder/scripts/tokenizer/detokenizer.perl'
 BPE_CODES='../exps/data/bpecodes'
