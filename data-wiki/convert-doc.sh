a2758b73464033b5e3eeb7752710e10f90f0d65f
leod <subtle.frustration@proton.me>
2019-03-22 14:36:39 +0100

    Use dirname in conversion script

--- data-wiki/convert-doc.sh
+++ data-wiki/convert-doc.sh
@@ -5,4 +5,4 @@ perl -pe 'BEGIN{undef $/;} s#<gallery>.*</gallery>##s' \
         -e 's/{{as of|\([^|}]*\)}}/as of \1/g' \
         -e 's/{{as of|\([^|]*\)|[^}]*}}/as of \1/g' \
 				-e 's/{{convert|\([^|]*\)|\([a-zA-Z]*\)[^}]*}}/\1 \2/g' \
-  | pandoc --wrap=none -f mediawiki --filter filter_markdown.py -t markdown
+  | pandoc --wrap=none -f mediawiki --filter $(dirname $0)/filter_markdown.py -t markdown
