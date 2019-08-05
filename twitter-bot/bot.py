9dafe2b56a329d47877c2b1fff41b27c6500dced
leod <subtle.frustration@proton.me>
2019-08-05 22:31:30 +0200

    Generate and tweet for a given ID

--- twitter-bot/bot.py
+++ twitter-bot/bot.py
@@ -1,21 +1,43 @@
 #!/usr/bin/env python3
 
-import tweepy
-from twitter import twitter_utils
+import sys
 import json
 import urllib
+import html
+
+import tweepy
+from twitter import twitter_utils
 
 HN_API_URL = 'https://hacker-news.firebaseio.com/v0/item/{}.json'
+HNCYNIC_API_URL = 'https://hncynic.leod.org/gen?title={}'
 MAX_TWEET_LEN = 280
 TWEET_TBC = '…'
 
 def get_hn_item_title(item_id):
   url = HN_API_URL.format(item_id)
-  reply = json.load(urllib.urlopen(url))
+  reply = json.load(urllib.request.urlopen(url))
   return reply['title']
 
+def get_hncynic_comments_and_scores(title):
+  url = HNCYNIC_API_URL.format(html.escape(title))
+  return json.load(urllib.request.urlopen(url))
+
+def score_comment(comment):
+  score = 1
+
+  if comment.endswith('@@'):
+    score = 0
+
+  return score
+
+def best_hncynic_comment(comments_and_scores):
+  assert len(comments_and_scores) > 0
+
+  comments = [entry[0] for entry in comments_and_scores]
+  return sorted(comments, key=score_comment)[-1]
+
 def postprocess(comment):
-  comment = comment.trim()
+  comment = comment.strip()
   comment = comment.replace('x86 _ 64', 'x86_64') \
                    .replace(': (', ' :(') \
                    .replace(': )', ' :)') \
@@ -49,8 +71,38 @@ def split_tweet(tweet, start_sep=TWEET_TBC, end_sep=TWEET_TBC):
   tweets.append(cur_tweet)
   return tweets
     
-def comment_tweet(item_id, title, comment):
-  return '"{}" (https://news.ycombinator.com/item?id={}\n{}'.format(title, item_id_comment)
+def tweet_text_for_comment(item_id, title, comment):
+  return '"{}" @ https://news.ycombinator.com/item?id={}\n\n{}'.format(title, item_id, comment)
+
+def send_tweet_thread(tweets, api):
+  prev_id = None
+  for tweet in tweets:
+    status = api.update_status(tweet, in_reply_to_status_id=prev_id)
+    prev_id = status.id
+
+def generate_and_tweet(item_id, api):
+  sys.stderr.write('Tweeting for item id: {}\n'.format(item_id))
+
+  title = get_hn_item_title(item_id)
+  sys.stderr.write('Received title: {}\n'.format(title))
+
+  comments_and_scores = get_hncynic_comments_and_scores(title)
+  sys.stderr.write('Generated comments: {}\n'.format(comments_and_scores))
+
+  best_comment = best_hncynic_comment(comments_and_scores)
+  sys.stderr.write('Best comment: {}\n'.format(best_comment))
+
+  best_comment_pp = postprocess(best_comment)
+  sys.stderr.write('Postprocessed: {}\n'.format(best_comment_pp))
+
+  tweet_text = tweet_text_for_comment(item_id, title, best_comment_pp)
+  sys.stderr.write('Tweet text: {}\n'.format(tweet_text))
+
+  split_tweets = split_tweet(tweet_text)
+  sys.stderr.write('Tweet thread: {}\n'.format(split_tweets))
+
+  send_tweet_thread(split_tweets, api)
+  sys.stderr.write('Done tweeting.\n')
 
 if __name__ == '__main__':
   with open('status.json') as f:
@@ -65,5 +117,5 @@ if __name__ == '__main__':
   api.verify_credentials()
 
   # Create a tweet
-  print(api.update_status('Hello Tweepy'))
+  generate_and_tweet(20617325, api)
 
